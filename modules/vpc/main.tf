644c3b09af4b8ce0b0a541129913bf2e1f846d11
aalok-akarsh <pandeyalok3663@gmail.com>
2026-03-28 10:29:36 +0000

    feat: initial project structure with module structure

---
+++ modules/vpc/main.tf
@@ -0,0 +1,18 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "automation-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
